2d2e6e8611dc2c0938eaa25e75e0897e980540e0
Bhavesh Borse <bborse344@gmail.com>
2025-03-26 19:55:27 +0530

    Delete LostAndFound directory

--- LostAndFound/apps/found/forms.py
+++
@@ -1,7 +0,0 @@
-# from django import form
-# from .models import Item
-
-# class ItemSubmission(form.ModelForm):
-#     class Meta:
-#         model = Item
-#         fields = ['item_name','foundAt','img']
